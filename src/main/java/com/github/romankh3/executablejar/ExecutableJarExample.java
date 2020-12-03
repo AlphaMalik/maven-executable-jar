f6e8577b61809b1998aa9ee3a7b18a87ea1d3cae
AlphaMalik <36862842+AlphaMalik@users.noreply.github.com>
2020-12-03 10:35:43 -0500

    Update ExecutableJarExample.java

--- src/main/java/com/github/romankh3/executablejar/ExecutableJarExample.java
+++ src/main/java/com/github/romankh3/executablejar/ExecutableJarExample.java
@@ -9,10 +9,12 @@ public class ExecutableJarExample {
 
     public static final String JAVA_REPOSITORY_TEMPLATE = "executable-jar-example";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello world, This is s a sample OpenShift/Kubernetes job");
         System.out.println("Value for the SAMPLE_ENV environment variable: " + System.getenv("SAMPLE_ENV"));
-        Integer exitValue = Integer.parseInt(System.getenv("EXIT_VALUE"));
-        System.exit(exitValue);
+        if (Boolean.parseBoolean(System.getenv("EXIT_VALUE"))
+            throw new Exception("Something failed, please check");
+            
+        System.out.println("sample OpenShift/Kubernetes job completed without any errors");
     }
 }
